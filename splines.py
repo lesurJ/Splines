704388b0e89994b8cfbc74da43328cd4d4d90e1f
lesurJ <lejeansur@gmail.com>
2023-03-16 11:45:11 +0100

    cleaned code; no more intermediary classes

--- splines.py
+++ splines.py
@@ -19,58 +19,44 @@ class Spline(object):
                 np.power(lin_sp, 3),
             )
         ).T
-        return self._get_spline(control_points, t_vector)
 
+        nb_sub_spline = (
+            len(control_points) // 3
+            if self.type == "shifting"
+            else len(control_points) - 3
+        )
+        shift = -3 if self.type == "shifting" else -1
 
-class FullSpline(Spline):
-    def __init__(self, characteristic_matrix):
-        super().__init__(characteristic_matrix)
-
-    def _get_spline(self, control_points, t_vector):
-        nb_sub_spline = len(control_points) // 3
         out = np.zeros(shape=(0, self.dim))
-
         for _ in range(nb_sub_spline):
             points = t_vector @ self.characteristic_matrix @ control_points[:4]
             out = np.vstack((out, points))
-            control_points = np.roll(control_points, shift=-3, axis=0)
+            control_points = np.roll(control_points, shift=shift, axis=0)
         return out
 
 
-class PartialSpline(Spline):
-    def __init__(self, characteristic_matrix):
-        super().__init__(characteristic_matrix)
-
-    def _get_spline(self, control_points, t_vector):
-        nb_sub_spline = len(control_points) - 3
-        out = np.zeros(shape=(0, self.dim))
-
-        for _ in range(nb_sub_spline):
-            points = t_vector @ self.characteristic_matrix @ control_points[:4]
-            out = np.vstack((out, points))
-            control_points = np.roll(control_points, shift=-1, axis=0)
-        return out
-
-
-class Bezier(FullSpline):
+class Bezier(Spline):
     def __init__(self):
         characteristic_matrix = np.array(
             [[1, 0, 0, 0], [-3, 3, 0, 0], [3, -6, 3, 0], [-1, 3, -3, 1]]
         )
         super().__init__(characteristic_matrix)
+        self.type = "shifting"
 
 
-class CatmullRom(PartialSpline):
+class CatmullRom(Spline):
     def __init__(self):
         characteristic_matrix = 0.5 * np.array(
             [[0, 2, 0, 0], [-1, 0, 1, 0], [2, -5, 4, -1], [-1, 3, -3, 1]]
         )
         super().__init__(characteristic_matrix)
+        self.type = "sliding"
 
 
-class B_spline(PartialSpline):
+class B_spline(Spline):
     def __init__(self):
         characteristic_matrix = (1 / 6) * np.array(
             [[1, 4, 1, 0], [-3, 0, 3, 0], [3, -6, 3, 0], [-1, 3, -3, 1]]
         )
         super().__init__(characteristic_matrix)
+        self.type = "sliding"
